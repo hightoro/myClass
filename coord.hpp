5ffc2f79be1a8eedad1c817b7db730a6248b4d09
hightoro <ayanokouji11@gmail.com>
2014-04-05 16:47:12 +0900

    a little changed

--- coord.hpp
+++ coord.hpp
@@ -25,7 +25,7 @@
 //template <class T> ostream& operator << ( ostream&, const coord& );
 
 // 五次元まで
-template <class T,int N>
+template <class T>
 class basic_coord
 {
 private:
@@ -41,30 +41,34 @@ public:
   /* ------------------------ *
    *|  Construct & Destruct  |*
    * ------------------------ */
-  basic_coord():                                  // Construct (Default)
+  basic_coord():                            // Construct (Default)
     dim(2),x(0),y(0),z(0),w(0),v(0){}
+
   basic_coord(T x_,T y_):
     dim(2),x(x_),y(y_),z(0),w(0),v(0){}     // 2
+
   basic_coord(T x_,T y_,T z_):
     dim(3),x(x_),y(y_),z(z_),w(0),v(0){}    // 3
+
   basic_coord(T x_,T y_,T z_,T w_):
     dim(4),x(x_),y(y_),z(z_),w(w_),v(0){}   // 4
+
   basic_coord(T x_,T y_,T z_,T w_,T v_):
     dim(5),x(x_),y(y_),z(z_),w(w_),v(v_){}  // 5
-  virtual ~basic_coord()=default;                 // Destruct
+
+  ~basic_coord()=default;                   // Destruct
 
   /* ------------------ *
    *|  Copy Construct  |*
    * ------------------ */
-  basic_coord( const basic_coord<T>& )=default;        // Copy Construct 1
+  basic_coord( const basic_coord<T>& )=default;              // Copy Construct 1
   basic_coord& operator=( const basic_coord<T>& )=default;   // Copy Construct 2
 
-  /* ----------------------- *
-   *|  Init & Copy & Clear  |*
-   * ----------------------- */
-  void init( );
-  void copy( const basic_coord<T>& );
-  void clear( );
+  /* ------------------ *
+   *|  Move Construct  |*
+   * ------------------ */
+  basic_coord( basic_coord<T>&& )=default;              // Move Construct 1
+  basic_coord& operator=( basic_coord<T>&& )=default;   // Move Construct 2
 
   /* ------- *
    *|  put  |*
@@ -86,13 +90,31 @@ public:
   /* --------------------- *
    *|  operator(compare)  |*
    * --------------------- */
-  int  comp( const basic_coord<T>& )const;
-  bool operator <  ( const basic_coord<T>& )const;
-  bool operator >  ( const basic_coord<T>& )const;
-  bool operator <= ( const basic_coord<T>& )const;
-  bool operator >= ( const basic_coord<T>& )const;
-  bool operator == ( const basic_coord<T>& )const;
-  bool operator != ( const basic_coord<T>& )const;
+  int  comp( const basic_coord<T>& obj )const
+  {
+    if( (v-obj.v)!=0 )
+      return (v-obj.v);
+    else if( (w-obj.w)!=0 )
+      return (w-obj.w);
+    else if( (z-obj.z)!=0 )
+      return (z-obj.z);
+    else if( (y-obj.y)!=0 )
+      return (y-obj.y);
+    else
+      return (x-obj.x);
+  }
+  bool operator <  ( const basic_coord<T>& obj )const
+  { return ( (this->comp(obj)) < 0 ); }
+  bool operator >  ( const basic_coord<T>& obj )const
+  { return ( (this->comp(obj)) > 0 ); }
+  bool operator <= ( const basic_coord<T>& obj )const
+  { return ( (this->comp(obj)) <= 0 ); }
+  bool operator >= ( const basic_coord<T>& obj )const
+  { return ( (this->comp(obj)) >= 0 ); }
+  bool operator == ( const basic_coord<T>& obj )const
+  { return ( (this->comp(obj)) == 0 ); }
+  bool operator != ( const basic_coord<T>& obj )const
+  { return ( (this->comp(obj)) != 0 ); }
 
   /* ----------------------- *
    *|  operator(increment)  |*
@@ -107,8 +129,10 @@ public:
    * --------------------- */
   basic_coord<T>& operator += ( const basic_coord<T>& );
   basic_coord<T>& operator -= ( const basic_coord<T>& );
-  basic_coord<T>& operator *= ( const basic_coord<T>& );
-  basic_coord<T>& operator /= ( const basic_coord<T>& );
+  //basic_coord<T>& operator *= ( const basic_coord<T>& );
+  basic_coord<T>& operator *= ( T );
+  //basic_coord<T>& operator /= ( const basic_coord<T>& );
+  basic_coord<T>& operator /= ( T& );
 
   /* -------------------- *
    *|  operator(binary)  |*
@@ -121,7 +145,7 @@ public:
   /* -------------------- *
    *|  operator(global)  |*
    * -------------------- */
-  friend ostream& operator << ( ostream& out, const basic_coord<T>& obj )
+  friend std::ostream& operator << ( ostream& out, const basic_coord<T>& obj )
   {
     if( obj.dim == 1 )
       out << boost::format("(%d)")%obj.x;
