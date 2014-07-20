29ae31d7d0f9c031b1f8595daa2dd267034d3eaa
hightoro <ayanokouji11@gmail.com>
2014-07-20 22:17:18 +0900

    add grid_map.hpp

--- gridmap.hpp
+++ gridmap.hpp
@@ -6,6 +6,14 @@
 
 #ifndef HIGHTORO_CPP_ORIGINAL_GRIDMAP_CLASS_H
 #define HIGHTORO_CPP_ORIGINAL_GRIDMAP_CLASS_H
+
+#include <iostream>
+#include <map>
+#include <exception>
+#include <stdexcept>
+
+#include "coord.hpp"
+
 //namespace pporig{
 
 template <typename T>
@@ -19,7 +27,10 @@ private:
   int max_x_;
   int max_y_;
 
-  std::map<coord<2>,T>
+  std::map<pporig::coord<2>,T> val_map_;
+
+  void check_arg()
+  { ((max_x_>min_x_)&&(max_y_>min_y_))?"":(throw std::invalid_argument("")); }
 
 public:
   /* ------------------------ *
@@ -29,11 +40,13 @@ public:
     min_x_(0),min_y_(0),max_x_(1),max_y_(1){}
   ~gridmap()=default;                   // Destruct
 
-  explicit constexpr gridmap(int x1,int y1):
-    min_x_(0),min_y_(0),max_x_(x1),max_y_(y1){}
+  explicit /*constexpr*/ gridmap(int x1,int y1):
+    min_x_(0),min_y_(0),max_x_(x1),max_y_(y1)
+  { this->check_arg(); }
 
-  explicit constexpr gridmap(int x1,int y1,int x0,int y0):
-    min_x_(x0),min_y_(y0),max_x_(x1),max_y_(y1){}
+  explicit /*constexpr*/ gridmap(int x1,int y1,int x0,int y0):
+    min_x_(x0),min_y_(y0),max_x_(x1),max_y_(y1)
+  { this->check_arg(); }
 
   /* ------------------ *
    *|  Copy Construct  |*
@@ -47,21 +60,26 @@ public:
   /*constexpr*/ gridmap( gridmap&& )=default;              // Move Construct 1
   /*constexpr*/ gridmap& operator=( gridmap&& )=default;   // Move Construct 2
 
-
   /* ---------- *
    *|  insert  |*
    * ---------- */
-
-
+  void insert(int x,int y,const T& val)
+  { val_map_.insert({ {x,y},val }); }
+  void insert(int x,int y,T&& val)
+  { val_map_.insert({ {x,y},val }); }
 
   /* ---------- *
    *|  erase  |*
    * ---------- */
-
+  void erase(int x,int y)
+  { val_map_.erase({x,y}); }
 
   /* --------- *
    *|  exist  |*
    * --------- */
+  bool exist(int x,int y)
+  { return (val_map_.count({x,y})!=0); }
+
 
 
 };
